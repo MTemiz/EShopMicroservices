4feb655cd3c24d8322ebe13201e8a9022f99957e
MTemiz <43716821+MTemiz@users.noreply.github.com>
2024-05-07 12:48:09 +0300

    changes

---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+
+namespace Catalog.API.Products.GetProducts
+{
+    public record GetProductsQuery : IQuery<GetProductsResult>;
+
+    public record GetProductsResult(IEnumerable<Product> products);
+
+    internal class GetProductsQueryHandler(IDocumentSession session, ILogger<GetProductsQueryHandler> logger) : IQueryHandler<GetProductsQuery, GetProductsResult>
+    {
+        public async Task<GetProductsResult> Handle(GetProductsQuery request, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductsQueryHandler.Handle called by with {@Query}", request);
+
+            var products = await session.Query<Product>().ToListAsync(cancellationToken);
+
+            return new GetProductsResult(products);
+        }
+    }
+}
+
