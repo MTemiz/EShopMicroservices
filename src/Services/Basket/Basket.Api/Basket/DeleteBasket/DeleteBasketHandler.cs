b808b6179b03a83310b24217d91219f2b680e50a
MTemiz <43716821+MTemiz@users.noreply.github.com>
2024-05-13 12:25:55 +0300

    feature implementasyonları

--- src/Services/Basket/Basket.Api/Basket/DeleteBasket/DeleteBasketHandler.cs
+++ src/Services/Basket/Basket.Api/Basket/DeleteBasket/DeleteBasketHandler.cs
@@ -1,1 +1,23 @@
+using FluentValidation;
+
 namespace Basket.Api.Basket.DeleteBasket;
+
+public record DeleteBasketCommand(string UserName) : ICommand<DeleteBasketResult>;
+
+public record DeleteBasketResult(bool IsSuccess);
+
+public class DeleteBasketCommandValidator : AbstractValidator<DeleteBasketCommand>
+{
+    public DeleteBasketCommandValidator()
+    {
+        RuleFor(x => x.UserName).NotNull().WithMessage("UserName is required");
+    }
+}
+
+public class DeleteBasketCommandHandler : ICommandHandler<DeleteBasketCommand, DeleteBasketResult>
+{
+    public async Task<DeleteBasketResult> Handle(DeleteBasketCommand command, CancellationToken cancellationToken)
+    {
+        return new DeleteBasketResult(true);
+    }
+}
