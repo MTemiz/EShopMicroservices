81f7bf503349e046b5f041a79cfc494c9049d0e1
MTemiz <43716821+MTemiz@users.noreply.github.com>
2024-06-15 14:20:37 +0300

    Ordering.Infrastructure DbContext eklendi.

---
+++ src/Services/Ordering/Ordering.Inftrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Models;
+
+namespace Ordering.Inftrastructure.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    { }
+
+    public DbSet<Customer> Customers => Set<Customer>();
+    public DbSet<Product> Products => Set<Product>();
+    public DbSet<Order> Orders => Set<Order>();
+    public DbSet<OrderItem> OrderItemss => Set<OrderItem>();
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+       builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+       
+        base.OnModelCreating(builder);
+    }
+
+}
